ca7f34e4319138abf75e4b59c2862f7c562a2c4e
Daniel Arteaga <71586064+D4nyArt@users.noreply.github.com>
2023-11-25 23:20:15 -0600

    Read "Grafo.txt" function

--- Grafo/src/Main.java
+++ Grafo/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String filePath = "Grafo/src/Grafo.txt";
+        List<List<Integer>> adjacencyMatrix = new ArrayList<>();
+
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+
+                String[] characters = line.split(" ");
+
+                List<Integer> row = new ArrayList<>();
+
+                for (String character: characters) {
+                    if (!character.trim().isEmpty()) {
+                        row.add(Integer.parseInt(character));
+                    }
+                }
+                adjacencyMatrix.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (List<Integer> row : adjacencyMatrix) {
+            for (Integer value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
     }
 }
