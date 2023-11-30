9a6521eeae87ddff3472b1ccf3ed37943fa06cd4
MrEstebato <tebananas@hotmail.com>
2023-11-30 12:21:00 -0600

    Final Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        PrecedenceGraph grafo = new PrecedenceGraph("src/Grafo.txt");
-        PrecedenceGraph grafo2 = new PrecedenceGraph("src/Grafo2.txt");
-        PrecedenceGraph grafo3 = new PrecedenceGraph("src/Grafo3.txt");
-        PrecedenceGraph grafo4 = new PrecedenceGraph("src/Grafo4.txt");
+        PrecedenceGraph grafo = new PrecedenceGraph("Grafo.txt");
+        PrecedenceGraph grafo2 = new PrecedenceGraph("Grafo2.txt");
+        PrecedenceGraph grafo3 = new PrecedenceGraph("Grafo3.txt");
+
+        // Grafo 1
         grafo.createPseudocode();
         System.out.println();
         grafo.showValidFlows();
+
+        // Grafo 2
+        System.out.println();
+        grafo2.createPseudocode();
+        System.out.println();
+        grafo2.showValidFlows();
+
+        // Grafo 3
+        System.out.println();
+        grafo3.createPseudocode();
+        System.out.println();
+        grafo3.showValidFlows();
     }
 }
