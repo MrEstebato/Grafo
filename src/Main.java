4456d170c87d771c11bed9be12349071014fe3d1
Daniel Arteaga <71586064+D4nyArt@users.noreply.github.com>
2023-11-30 01:05:23 -0600

    Added comments(no delete node and pseudo)

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,15 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public static ArrayList<ArrayList<Integer>> matrixReader(String filePath) {
-        ArrayList<ArrayList<Integer>> adjacencyMatrix = new ArrayList<>();
-        try (FileReader fileReader = new FileReader(filePath);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] characters = line.split(" ");
-                ArrayList<Integer> row = new ArrayList<>();
-                for (String character : characters) {
-                    if (!character.trim().isEmpty()) {
-                        row.add(Integer.parseInt(character));
-                    }
-                }
-                adjacencyMatrix.add(row);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return adjacencyMatrix;
-    }
-
-    public static void showValidFlows(precedenceGraph grafo, Node nodoActual, List<Node> caminoActual) {
-        caminoActual.add(nodoActual);
-
-        if (nodoActual.children.isEmpty()) {
-
-            for (Node node : caminoActual) {
-                System.out.print(node.getData() + "; ");
-            }
-            System.out.println();
-        } else {
-            for (Node hijo : nodoActual.children) {
-                showValidFlows(grafo, hijo, new ArrayList<>(caminoActual));
-            }
-        }
-    }
     public static void main(String[] args) {
-        String filePath = "Grafo.txt";
-        ArrayList<ArrayList<Integer>> adjacencyMatrix = matrixReader(filePath);
-        precedenceGraph grafo = new precedenceGraph();
-        int nodeCounter = 1;
-        for(int i=0; i<adjacencyMatrix.get(0).size(); i++){
-            Node addedNode = new Node("S"+nodeCounter);
-            grafo.addNode(addedNode);
-            nodeCounter++;
-        }
-
-        for(int j=0; j<grafo.getTotalNodes(); j++){
-            for(int k=0; k<adjacencyMatrix.get(0).size(); k++){
-                if(adjacencyMatrix.get(j).get(k) == 1){
-                    grafo.addEdge(grafo.nodes.get(j), grafo.nodes.get(k));
-                }
-            }
-        }
-
-        /*for(int x=0; x<grafo.getTotalNodes(); x++){
-            System.out.println("Children of " + grafo.nodes.get(x).getData() + ": ");
-            for(Node node: grafo.nodes.get(x).children){
-                System.out.println(node.getData());
-            }
-        }*/
-
-        showValidFlows(grafo, grafo.nodes.getFirst(), new ArrayList<>());
+        PrecedenceGraph grafo = new PrecedenceGraph("src/Grafo.txt");
+        PrecedenceGraph grafo2 = new PrecedenceGraph("src/Grafo2.txt");
+        PrecedenceGraph grafo3 = new PrecedenceGraph("src/Grafo3.txt");
+        PrecedenceGraph grafo4 = new PrecedenceGraph("src/Grafo4.txt");
+        grafo.showValidFlows();
+        System.out.println();
+        grafo2.showValidFlows();
+        System.out.println();
+        grafo3.showValidFlows();
+        System.out.println();
+        grafo4.showValidFlows();
     }
-
 }
